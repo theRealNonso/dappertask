f738036f4091de2919301c7816a1e51fd33c419a
nonso <nonsookoroafor@gmail.com>
2018-08-06 18:40:12 +0100

    initial commit

---
+++ config/urls.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.views import defaults as default_views
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "about/",
+        TemplateView.as_view(template_name="pages/about.html"),
+        name="about",
+    ),
+    # Django Admin, use {% url 'admin:index' %}
+    path(settings.ADMIN_URL, admin.site.urls),
+    # User management
+    path(
+        "users/",
+        include("dappertask.users.urls", namespace="users"),
+    ),
+    # Your stuff: custom urls includes go here
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
